8b0b9205425efcfe54f3242329d71aea4d885113
giackperetti <github.giack@gmail.com>
2024-06-29 12:01:58 +0200

    added file for numeric pin bruteforcing

---
+++ pin.py
@@ -0,0 +1,33 @@
+import time
+
+
+def verify_pin(pin, pin_corretto):
+    return pin == pin_corretto
+
+
+def brute_force_pin(pin_corretto):
+    for i in range(1000000):
+        pin_tentativo = f"{i:06d}"
+        if verify_pin(pin_tentativo, pin_corretto):
+            return pin_tentativo
+    return None
+
+
+def main():
+    pin_corretto = "745632"
+    start_time = time.time()
+
+    pin_trovato = brute_force_pin(pin_corretto)
+
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+
+    if pin_trovato:
+        print(f"PIN code found: {pin_trovato}")
+        print(f"Elapsed time: {elapsed_time:.6f} s")
+    else:
+        print("PIN not found")
+
+
+if __name__ == "__main__":
+    main()
